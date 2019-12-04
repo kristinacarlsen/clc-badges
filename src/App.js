b868e2b1b3cef59af2030cf7dd8719c023808939
kristinacarlsen <kristida84@gmail.com>
2019-12-04 10:44:25 -0700

    adds basic contact form

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Footer from './components/Footer';
 import Home from './pages/home-page';
 import Shop from './pages/shop';
 import About from './pages/about';
+import Contact from './pages/contact';
 
 function App() {
   return (
@@ -23,7 +24,7 @@ function App() {
         <Route exact path='/' component={Home} />
         <Route exact path='/shop' component={Shop} />
         <Route exact path='/about' component={About} />
-
+        <Route exact path='/contact' component={Contact} />
       <Footer />
     </div>
     </Router>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,48 @@
 import React, { Component } from "react";
+import '../assets/pages.css';
 
 class Contact extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fname: '',
+            lname: '',
+            email: '',
+            message: '',
+            mailSent: false,
+            error: null
+        }
+    }
+
+    handleFormSubmit(event) {
+        event.preventDefault();
+        console.log(this.state);
+    }
+
     render() {
         return (
-            <div className="container">
-                <h1>Contact Ciny</h1>
+            <div className="contactForm">
+                <h1>Contact Cindy</h1>
+
                 <form action="/" className="contact">
+
                 <label>First Name</label>
-                <input type="text" className='fname' name='firstName' placeholder='first name'/>
+                <input type="text" className='fname' name='firstName' placeholder='first name' value={this.state.fname} onChange={e => this.setState({fname: e.target.value})}/>
+
+                <label>Last Name</label>
+                <input type="text" className='lname' name="lastName" placeholder='last name' value={this.state.lname} onChange={e => this.setState({lname: e.target.value})}/>
+
+                <label>Email</label>
+                <input type="email" className='email' name='email' placeholder='your email' value={this.state.email} onChange={e=> this.setState({email: e.target.value})}/>
+
+                <textarea name="message" className="message" cols="30" rows="10" placeholder='Write your message' value={this.state.message} onChange={e => this.setState({message: e.target.value})}></textarea>
+
+                <input type="submit" onClick={e => this.handleFormSubmit(e)} value='Send'/>
                 </form>
             
             </div>
         )
     }
 }
+
+export default Contact;
