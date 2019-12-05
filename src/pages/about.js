78dd27338049ecaa3a6c5767d977b10ec0966dc6
kristinacarlsen <kristida84@gmail.com>
2019-12-05 00:10:01 -0700

    adds about page, and contact page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import '../assets/App.css';
+import '../assets/pages.css';
 
 const About = () => (
-    <div className="container">
+    <div className="abtContainer">
         <h1>About Cindy</h1>
 
         <p>Cindy has been a nurse for 30 years. She started collecting the tabs that seal medication, and created ironic badges for herself and her co-workers while working in pediatrics.</p>
