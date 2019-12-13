73f39b34a0d69ae0f98dea7d9e41a05f316b08c4
kristinacarlsen <kristida84@gmail.com>
2019-12-12 22:21:47 -0700

    Updates

---
+++ precache-manifest.2ed50ce937132fb4e72af67a2e59ee23.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2e81ae3b27210abeb266bbd23d6592e",
+    "url": ":http://kristinacarlsen.github.io/clc-badges/index.html"
+  },
+  {
+    "revision": "e728990c04b839618634",
+    "url": ":http://kristinacarlsen.github.io/clc-badges/static/css/main.c93d81d9.chunk.css"
+  },
+  {
+    "revision": "8fb7439e681db394f532",
+    "url": ":http://kristinacarlsen.github.io/clc-badges/static/js/2.54c26eff.chunk.js"
+  },
+  {
+    "revision": "e728990c04b839618634",
+    "url": ":http://kristinacarlsen.github.io/clc-badges/static/js/main.ba2c6d8e.chunk.js"
+  },
+  {
+    "revision": "a84f46fc7a87675c61e8",
+    "url": ":http://kristinacarlsen.github.io/clc-badges/static/js/runtime-main.59e71b3a.js"
+  },
+  {
+    "revision": "c245e48c4c4d3de461e1e88bc77aa4c2",
+    "url": ":http://kristinacarlsen.github.io/clc-badges/static/media/CLC.c245e48c.png"
+  },
+  {
+    "revision": "b64a33d453225839a1d875a12f0d0b91",
+    "url": ":http://kristinacarlsen.github.io/clc-badges/static/media/CLCLogo.b64a33d4.png"
+  },
+  {
+    "revision": "d42626f60fd9e80c7a7e32413c1cc389",
+    "url": ":http://kristinacarlsen.github.io/clc-badges/static/media/candycane.d42626f6.jpg"
+  },
+  {
+    "revision": "019d95acd6de433a94b7f82ef5acdf09",
+    "url": ":http://kristinacarlsen.github.io/clc-badges/static/media/penguin.019d95ac.jpg"
+  },
+  {
+    "revision": "21e782d1bee5b5617140455c85061e9d",
+    "url": ":http://kristinacarlsen.github.io/clc-badges/static/media/poinsettia.21e782d1.jpg"
+  },
+  {
+    "revision": "abd4c8acca6c7cee3451148694aa1ccb",
+    "url": ":http://kristinacarlsen.github.io/clc-badges/static/media/spider.abd4c8ac.jpg"
+  },
+  {
+    "revision": "453a9701fdb6b323fa3849ec18d7c63c",
+    "url": ":http://kristinacarlsen.github.io/clc-badges/static/media/treeB.453a9701.jpg"
+  },
+  {
+    "revision": "1a6d957abd30c986a6313aafccfa3ab8",
+    "url": ":http://kristinacarlsen.github.io/clc-badges/static/media/turkey.1a6d957a.jpg"
+  }
+]);
